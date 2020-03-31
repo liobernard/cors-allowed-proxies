9da822285d42db1dbd6f713f33a257f1974217da
Ubuntu <ubuntu@ip-172-31-88-40.ec2.internal>
2020-03-31 18:09:38 +0000

    Configure CORS-allowed urls.

--- cors_allowed_proxies/settings.py
+++ cors_allowed_proxies/settings.py
@@ -116,7 +116,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_METHODS = ['GET', 'HEAD', 'OPTIONS',]
-CORS_URLS_REGEX = config('CORS_URLS_REGEX')
+CORS_URLS_REGEX = r'^\/(proxies|proxies\/csv|proxy)\/?$'
 
 CACHES = {
     'default': {
